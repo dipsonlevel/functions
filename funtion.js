55e5e5d1ff0cb9f73a5e8fdb6d8b031d8c5c8768
dipson level <dipsonlevel@gmail.com>
2024-06-19 17:22:05 +0100

    first commit

---
+++ funtion.js
@@ -0,0 +1,13 @@
+
+function areaofaArectangle(a,b){
+    console.log(a*b)
+}
+areaofaArectangle(3,5)
+
+
+function calculateAreaOfRectangle(length, width) {
+    const area = length * width
+    console.log(`The area of the rectangle is: ${area}`)
+  }
+  calculateAreaOfRectangle(3,6)
+  calculateAreaOfRectangle(6,5)
